36baa982f93b955fc3e8f7ce2059fbf807c00f2c
mkoner <amzid@hotmail.fr>
2024-11-01 14:55:34 -0500

    Running basic thread implementing Runnable

--- threads/src/main/java/mkoner/basicMT/RunnableImplementation.java
+++ threads/src/main/java/mkoner/basicMT/RunnableImplementation.java
@@ -2,6 +2,7 @@ package mkoner.basicMT;
 
 public class RunnableImplementation {
     public static void main(String[] args) {
+        Thread t0 = new Thread(new MyThread());
         Thread t1 = new Thread(()->{
             for(int i=0; i<10; i++){
                 System.out.println(Thread.currentThread().getName()+" "+i);
@@ -13,11 +14,21 @@ public class RunnableImplementation {
             }
         });
 
+        t0.start();
         t1.start();
         t2.start();
         /*
         The seq of execution of the threads depends on the scheduler anytime it finds
         that they are available resources to run a thread it is allocated execution time.
          */
+    }
+}
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            System.out.println(Thread.currentThread().getName()+" "+i);
+        }
     }
 }
